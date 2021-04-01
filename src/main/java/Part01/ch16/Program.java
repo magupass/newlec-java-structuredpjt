51aac00da9f035d6efc78ae3fbb1bb1d407a9a2a
Edward <magupass@gmail.com>
2021-04-01 23:05:52 +0900

    4월1일 서울집

---
+++ src/main/java/Part01/ch16/Program.java
@@ -0,0 +1,9 @@
+package Part01.ch16;
+
+public class Program {
+    public static void main(String[] args) {
+        System.out.write('A'+9);
+        System.out.write('A'+100);
+        System.out.flush();
+    }
+}
