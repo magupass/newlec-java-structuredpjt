d9de345fdd13e5c231defbd9f3cca833b85626b3
wcjeong <wcjeong@hist.co.kr>
2021-04-02 10:13:41 +0900

    4월 2일 첫번째

---
+++ src/main/java/Part01/ch19/Program.java
@@ -0,0 +1,29 @@
+package Part01.ch19;
+
+public class Program {
+    public static void main(String[] args) {
+        int kor1, kor2, kor3;
+        int total;
+        float avg;
+
+        kor1 = 55;
+        kor2 = 65;
+        kor3 = 77;
+
+        total = kor1 + kor2 + kor3;
+        avg = total / 3.00f;
+
+        /*성적 출력 부분*/
+        System.out.println("┌────────────────────────────────────┐");
+        System.out.println("│              성적 출력               │");
+        System.out.println("└────────────────────────────────────┘");
+
+        System.out.printf("\t국어1: %d\n",kor1);
+        System.out.printf("\t국어1: %d\n",kor2);
+        System.out.printf("\t국어1: %d\n",kor3);
+        System.out.printf("\t총점: %d\n",total);
+        System.out.printf("\t평균: %.2f\n",avg);
+        System.out.println("──────────────────────────────────────");
+
+    }
+}
