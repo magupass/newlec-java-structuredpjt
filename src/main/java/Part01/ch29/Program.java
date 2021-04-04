53d88f225094bff8de8d1e4bfd9e0f3724a8b9d8
dev1 <dev1@dev.com>
2021-04-04 21:26:40 +0900

    4월4일 first

---
+++ src/main/java/Part01/ch29/Program.java
@@ -0,0 +1,87 @@
+package Part01.ch29;
+
+import java.util.Scanner;
+
+//ch31 이전 버전
+public class Program {
+    public static void main(String[] args) {
+        int kor1 = 0;
+        int kor2 = 0;
+        int kor3 = 0;
+        int[] kors = {kor1, kor2, kor3} ;
+        int total;
+        float avg;
+
+        Scanner scan = new Scanner(System.in);
+
+//        kor1 = 0;
+//        kor2 = 0;
+//        kor3 = 0;
+
+        while (true){
+
+            /*성적 입력 부분*/
+            System.out.println("┌────────────────────────────────────┐");
+            System.out.println("│              성적 입력               │");
+            System.out.println("└────────────────────────────────────┘");
+
+            do {
+                System.out.print("\t국어1:");
+                kor1 = scan.nextInt();
+                if(kor1 < 0 || 100 < kor1)
+                    System.out.println("성적범위(0~100)를 벗어났습니다.");
+            }while (kor1 < 0 || 100 < kor1);
+            do {
+                System.out.print("\t국어2:");
+                kor2 = scan.nextInt();
+                if(kor2 < 0 || 100 < kor2)
+                    System.out.println("성적범위(0~100)를 벗어났습니다.");
+            }while (kor2 < 0 || 100 < kor2);
+            do {
+                System.out.print("\t국어3:");
+                kor3 = scan.nextInt();
+                if(kor3 < 0 || 100 < kor3)
+                    System.out.println("성적범위(0~100)를 벗어났습니다.");
+            }while (kor3 < 0 || 100 < kor3);
+
+            //성적 출력 부분
+            total = kor1 + kor2 + kor3;
+            avg = total / 3.0f;
+
+            System.out.println("┌────────────────────────────────────┐");
+            System.out.println("│              성적 출력               │");
+            System.out.println("└────────────────────────────────────┘");
+
+//            for (int i = 0; i < 3; i++){
+//                System.out.printf("\t국어%d : %3d\n", i+1, kor1);
+//            }
+
+            System.out.println("KORS[i]의 값은: " + kors[1]);
+            for (int i = 0; i < 3; i++){
+                System.out.printf("\t국어%d : %3d\n", i+1, kors[i]);
+            }
+
+//            System.out.printf("\t국어1: %d\n",kor1);
+//            System.out.printf("\t국어1: %d\n",kor2);
+
+//            System.out.printf("\t국어1: %d\n",kor3);
+            System.out.printf("\t총점 : %3d\n", total);
+            System.out.printf("\t평균 : %6.2f\n",avg);
+            System.out.println("--------------------------------------");
+        }
+
+//
+//        /*성적 출력 부분*/
+//        System.out.println("┌────────────────────────────────────┐");
+//        System.out.println("│              성적 출력               │");
+//        System.out.println("└────────────────────────────────────┘");
+//
+//        System.out.printf("\t국어1: %d\n",kor1);
+//        System.out.printf("\t국어1: %d\n",kor2);
+//        System.out.printf("\t국어1: %d\n",kor3);
+//        System.out.printf("\t총점: %d\n",total);
+//        System.out.printf("\t평균: %.2f\n",avg);
+//        System.out.println("──────────────────────────────────────");
+
+    }
+}
