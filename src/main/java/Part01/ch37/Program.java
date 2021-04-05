83de837af834e90a424776878e3a64dd1ca3b073
Edward <magupass@gmail.com>
2021-04-05 23:00:39 +0900

    4월5일 두번째

--- src/main/java/Part01/ch37/Program.java
+++ src/main/java/Part01/ch37/Program.java
@@ -1,12 +1,12 @@
-package Part01.ch36;
+package Part01.ch37;
 
 import java.util.Scanner;
 
-//다차원배열로 변경1
+//다차원배열로 변경2
 public class Program {
     public static void main(String[] args) {
 
-        int[] kors = new int[3*3];
+        int[][] kors = new int[3][3];
         int total;
         float avg;
         int menu;
@@ -15,7 +15,7 @@ public class Program {
 
         for (int j = 0; j < 3; j++){
             for (int i = 0; i < 3; i++){
-                kors[3*j+i]=0;
+                kors[j][i]=0;
             }
         }
 
@@ -43,10 +43,10 @@ public class Program {
                         for (int i = 0; i < 3; i++){
                             do {
                                 System.out.printf("\t%d학년 국어%d:",j+1,i+1);
-                                kors[3*j+i] = scan.nextInt();
-                                if(kors[3*j+i] < 0 || 100 < kors[3*j+i])
+                                kors[j][i] = scan.nextInt();
+                                if(kors[j][i] < 0 || 100 < kors[j][i])
                                     System.out.println("성적범위(0~100)를 벗어났습니다.");
-                            }while (kors[3*j+i] < 0 || 100 < kors[3*j+i]);
+                            }while (kors[j][i] < 0 || 100 < kors[j][i]);
                         }
                     }
 
@@ -64,9 +64,9 @@ public class Program {
                     for (int j = 0; j<3; j++){
                         System.out.printf("<%d학년 국어성적>--------------\n",j+1);
                         for (int i = 0; i < 3; i++){
-                            System.out.printf("\t%d학년 국어%d : %3d\n",j+1, i+1, kors[3*j+i]);
+                            System.out.printf("\t국어%d : %3d\n",i+1, kors[j][i]);
                         }
-                        total = kors[3*j+0] + kors[3*j+1] + kors[3*j+2];
+                        total = kors[j][0] + kors[j][1] + kors[j][2];
                         avg = total / 3.0f;
                         System.out.printf("\t총점 : %3d\n", total);
                         System.out.printf("\t평균 : %6.2f\n",avg);
